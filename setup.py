b12fb3481046b9b9bc41037685cc91929d500be5
Artem Ustinov <artem.ustinov@gmail.com>
2018-12-18 14:02:55 -0800

    Update setup.py File.
    - Use wavefront-sdk-python from PyPI
    - Updated the version specs

--- setup.py
+++ setup.py
@@ -1,37 +1,35 @@
 # coding: utf-8
 
+"""Wavefront Pyformance Library
+
+        The Wavefront Pyformance library provides Wavefront reporters
+        (via proxy and direct ingestion) and a simple abstraction for
+        tagging at the host level.
+        It also includes support for Wavefront delta counters.
 """
-    Wavefront Pyformance Library
-
-    <p>The Wavefront Pyformance library provides Wavefront reporters (via proxy and direct ingestion) and a simple abstraction for tagging at the host level. It also includes support for Wavefront delta counters.</p>  # noqa: E501
-
-"""
 
 
-from setuptools import setup, find_packages  # noqa: H301
+try:
+    import setuptools
+except ImportError:
+    import sys
+    sys.exit('Failed to import setuptools. Do pip install setuptools first.')
 
-NAME = "wavefront_pyformance"
-VERSION = "0.9.2"
-# To install the library, run the following
-#
-# python setup.py install
-#
-# prerequisite: setuptools
-# http://pypi.python.org/pypi/setuptools
 
-REQUIRES = ["pyformance >= 0.4", "wavefront_python_sdk"]
-
-setup(
-    name=NAME,
-    version=VERSION,
-    description="Wavefront Pyformance Library",
-    author_email="",
-    url="https://github.com/wavefrontHQ/wavefront-pyformance",
-    keywords=["Wavefront Pyformance", "Wavefront"],
-    install_requires=REQUIRES,
-    packages=find_packages(),
+setuptools.setup(
+    name='wavefront_pyformance',
+    version='0.9.2',
+    description='Wavefront Pyformance Library',
+    author_email='chitimba@wavefront.com',
+    url='https://github.com/wavefrontHQ/wavefront-pyformance',
+    keywords=('Pyformance', 'Wavefront Pyformance', 'Wavefront'),
+    install_requires=('pyformance>=0.4', 'wavefront-sdk-python>=1.0'),
+    packages=setuptools.find_packages(exclude=('tests',)),
     include_package_data=True,
-    long_description="""\
-    The Wavefront Pyformance library provides Wavefront reporters (via proxy and direct ingestion) and a simple abstraction for tagging at the host level. It also includes support for Wavefront delta counters.
+    long_description="""
+        The Wavefront Pyformance library provides Wavefront reporters
+        (via proxy and direct ingestion) and a simple abstraction for
+        tagging at the host level.
+        It also includes support for Wavefront delta counters.
     """
 )
