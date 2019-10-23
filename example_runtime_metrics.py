0aa639f2525dc072210db2cda0761a52b3cf6981
sjaiswalvmware <sjaiswal@vmware.com>
2019-10-23 16:15:49 +0530

    more fixes for Travis checks issues

--- example_runtime_metrics.py
+++ example_runtime_metrics.py
@@ -1,53 +1,47 @@
 """Python Runtime Metric Collection Example."""
 
 
-import sys
+from wavefront_pyformance import tagged_registry
+from wavefront_pyformance import wavefront_reporter
+
+
 import argparse
+import sys
 import time
 
 
 sys.path.insert(1, '../')
 
 
-from wavefront_pyformance import tagged_registry
-from wavefront_pyformance import wavefront_reporter
-
-
 def report_metrics(host, server='', token=''):
-    """Runtime Metric Reporting Function Example"""
+    """Runtime Metric Reporting Function Example."""
     reg = tagged_registry.TaggedRegistry()
-
-
     wf_proxy_reporter = wavefront_reporter.WavefrontProxyReporter(
         host=host, port=2878, registry=reg,
         source='runtime-metric-test',
         tags={'global_tag1': 'val1', 'global_tag2': 'val2'},
         prefix='python.runtime.',
         runtime_metric=True).report_minute_distribution()
-
-
-    #wf_direct_reporter = wavefront_reporter.WavefrontDirectReporter(
+    # wf_direct_reporter = wavefront_reporter.WavefrontDirectReporter(
     #    server=server, token=token, registry=reg,
     #    source='runtime-metric-test',
     #    tags={'global_tag1': 'val1', 'global_tag2': 'val2'},
     #    prefix='python.runtime.',
     #    runtime_metric=False).report_minute_distribution()
-
-
     wf_proxy_reporter.report_now()
     wf_proxy_reporter.stop()
-    #wf_direct_reporter.report_now()
-    #wf_direct_reporter.stop()
+    # wf_direct_reporter.report_now()
+    # wf_direct_reporter.stop()
 
 
 if __name__ == '__main__':
     # python example_runtime_metrics.py proxy_host server_url server_token
     arg = argparse.ArgumentParser()
     arg.add_argument('host', help='Wavefront proxy host name.')
-    #arg.add_argument('server', help='Wavefront server for direct ingestion.')
-    #arg.add_argument('token', help='Wavefront API token.')
+    # arg.add_argument('server', help='Wavefront server for direct ingestion.')
+    # arg.add_argument('token', help='Wavefront API token.')
     ARGS = arg.parse_args()
     while True:
         report_metrics(ARGS.host)
-        #report_metrics(ARGS.host, ARGS.server, ARGS.token)
+        # report_metrics(ARGS.host, ARGS.server, ARGS.token)
         time.sleep(30)
