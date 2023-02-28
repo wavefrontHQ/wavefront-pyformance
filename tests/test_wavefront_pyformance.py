068cd87030187901d528c280fc4d06447ab338a7
Artem Ustinov <austinov@vmware.com>
2023-02-28 13:53:50 -0800

    Fix `flake8` Errors

--- tests/test_wavefront_pyformance.py
+++ tests/test_wavefront_pyformance.py
@@ -14,30 +14,30 @@ class TestDelta(unittest.TestCase):
         """Test Delta Counter."""
         reg = tagged_registry.TaggedRegistry()
         counter = delta.delta_counter(reg, 'foo')
-        assert(isinstance(counter, delta.DeltaCounter))
+        assert isinstance(counter, delta.DeltaCounter)
 
         # test duplicate (should return previously registered counter)
         duplicate_counter = delta.delta_counter(reg, 'foo')
-        assert(counter == duplicate_counter)
-        assert(delta.is_delta_counter(delta.DeltaCounter.DELTA_PREFIX + 'foo',
-                                      reg))
+        assert counter == duplicate_counter
+        assert delta.is_delta_counter(delta.DeltaCounter.DELTA_PREFIX + 'foo',
+                                      reg)
         different_counter = delta.delta_counter(reg, 'foobar')
-        assert(counter != different_counter)
+        assert counter != different_counter
 
     def test_has_delta_prefix(self):
         """Test Delta Prefix Existence."""
-        assert(delta._has_delta_prefix(
-            delta.DeltaCounter.DELTA_PREFIX + 'foo'))  # valid prefix
-        assert(delta._has_delta_prefix(
-            delta.DeltaCounter.ALT_DELTA_PREFIX + 'foo'))  # valid prefix
-        assert(delta._has_delta_prefix('foo') is False)  # invalid prefix
+        assert delta._has_delta_prefix(
+            delta.DeltaCounter.DELTA_PREFIX + 'foo')  # valid prefix
+        assert delta._has_delta_prefix(
+            delta.DeltaCounter.ALT_DELTA_PREFIX + 'foo')  # valid prefix
+        assert delta._has_delta_prefix('foo') is False  # invalid prefix
 
     def test_get_delta_name(self):
         """Test Getting Delta Name."""
         d = delta.get_delta_name('delta.prefix',
                                  delta.DeltaCounter.DELTA_PREFIX + 'foo',
                                  'count')
-        assert(d.startswith(delta.DeltaCounter.DELTA_PREFIX))
+        assert d.startswith(delta.DeltaCounter.DELTA_PREFIX)
 
     def test_wavefront_histogram(self):
         """Test Wavefront Histogram."""
@@ -45,8 +45,8 @@ class TestDelta(unittest.TestCase):
         _ = reg.histogram('pyformance_hist').add(1.0)
         wavefront_hist = wavefront_histogram.wavefront_histogram(
             reg, 'wavefront_hist').add(2.0)
-        assert(isinstance(wavefront_hist,
-                          wavefront_histogram.WavefrontHistogram))
+        assert isinstance(wavefront_hist,
+                          wavefront_histogram.WavefrontHistogram)
 
 
 if __name__ == '__main__':
